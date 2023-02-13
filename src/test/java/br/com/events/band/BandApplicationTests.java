aa73fc50f271d8122a441ad1fa604863ae85c7fe
gabrielga-dev <dealmeida.gabriel.d@gmail.com>
2023-02-13 20:22:25 -0300

    first commit

---
+++ src/test/java/br/com/events/band/BandApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.events.band;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
