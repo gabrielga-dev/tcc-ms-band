b2ac24764be2fb66e1e78dc7bc01f45ceb0c920d
gabrielga-dev <dealmeida.gabriel.d@gmail.com>
2023-11-23 18:59:40 -0300

    fix/202311231858-block-feature-by-role: now, feature will bo only accessed by persons that has the correct role

--- src/main/java/br/com/events/band/adapter/port/rest/v1/MusicianTypeRestControllerV1.java
+++ src/main/java/br/com/events/band/adapter/port/rest/v1/MusicianTypeRestControllerV1.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class MusicianTypeRestControllerV1 implements MusicianTypePort {
 
     @Override
     @GetMapping
+    @PreAuthorize("hasAnyAuthority('BAND', 'CONTRACTOR')")
     public ResponseEntity<Page<MusicianTypeResponse>> findByCriteria(
             Pageable pageable, MusicianTypeCriteria criteria
     ) {
