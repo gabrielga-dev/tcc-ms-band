a59d89b7b34fb15673656604d5eecffc903b8ea0
gabrielga-dev <dealmeida.gabriel.d@gmail.com>
2024-03-06 19:14:16 -0300

    feature/ME202403061853: now we can edit musics

---
+++ src/main/java/br/com/events/band/business/use_case/music/impl/FindMusicByUuidUseCaseImpl.java
@@ -0,0 +1,29 @@
+package br.com.events.band.business.use_case.music.impl;
+
+import br.com.events.band.business.command.music.FindMusicCommand;
+import br.com.events.band.business.use_case.music.FindMusicByUuidUseCase;
+import br.com.events.band.core.exception.band.BandOwnerException;
+import br.com.events.band.core.exception.music.MusicNonExistenceException;
+import br.com.events.band.core.util.AuthUtil;
+import br.com.events.band.data.io.music.response.MusicResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class FindMusicByUuidUseCaseImpl implements FindMusicByUuidUseCase {
+
+    private final FindMusicCommand findMusicCommand;
+
+    @Override
+    public MusicResponse execute(String uuid) {
+        var music = findMusicCommand.byUuid(uuid)
+                .orElseThrow(MusicNonExistenceException::new);
+
+        if (!AuthUtil.getAuthenticatedPersonUuid().equals(music.getContributingBand().getOwnerUuid())) {
+            throw new BandOwnerException();
+        }
+
+        return new MusicResponse(music);
+    }
+}
